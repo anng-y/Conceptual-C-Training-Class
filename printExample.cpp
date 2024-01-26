cc3e98a44765ca65b80f57cfd269c67e8eec85a5
anng-y <anngyip59@gmail.com>
2024-01-26 11:18:03 -0800

    first commit

---
+++ printExample.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string name = "everyone";
+    std::cout << "Hello " << name << "!\n";
+    return 0;
+}
---
+++ sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("sizeof(char) = %lu\n", sizeof(char));
+    printf("sizeof(int) = %lu\n", sizeof(int));
+    printf("sizeof(float) = %lu\n", sizeof(float));
+    printf("sizeof(double) = %lu\n", sizeof(double));
+    return 0;
+}
